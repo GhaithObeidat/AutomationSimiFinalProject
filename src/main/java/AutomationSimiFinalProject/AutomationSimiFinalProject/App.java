7d3bffada51bd50297eaedd8cb854c00b5558e80
GhaithObeidat <ghaith.ob85@gmail.com>
2025-04-30 00:50:28 +0300

    update

---
+++ src/main/java/AutomationSimiFinalProject/AutomationSimiFinalProject/App.java
@@ -0,0 +1,10 @@
+package AutomationSimiFinalProject.AutomationSimiFinalProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
